d6a8b70b40e2ad722e8f4307a42bbd61ead99038
Cornelius Bopp <Cornelius.Bopp@gmx.de>
2023-06-06 17:45:00 +0200

    added but not yet integrated customEdgeStyleClass

--- src/graph/loadGraph.tsx
+++ src/graph/loadGraph.tsx
@@ -26,7 +26,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import { DefaultGraph, DefaultLabelStyle, EdgeRouter, INode, Rect, ShapeNodeShape, ShapeNodeStyle, Stroke, FreeNodeLabelModel, Font, FontWeight, VerticalTextAlignment, HorizontalTextAlignment, TextWrapping, LabelShape, BorderLine, FontStyle, InteriorStretchLabelModel, InteriorStretchLabelModelPosition} from 'yfiles'
+import { DefaultGraph, DefaultLabelStyle, EdgeRouter, INode, Rect, ShapeNodeShape, ShapeNodeStyle, Stroke, FreeNodeLabelModel, Font, FontWeight, VerticalTextAlignment, HorizontalTextAlignment, TextWrapping, LabelShape, BorderLine, FontStyle, InteriorStretchLabelModel, InteriorStretchLabelModelPosition, EdgeDefaults, IEdgeDefaults, IEdgeStyle, EdgeStyleBase, IEdge, IRenderContext, Visual, SvgVisual, PolylineEdgeStyle, Arrow} from 'yfiles'
 import uTop40 from './data/top40.json'
 import uTop100 from './data/top100.json'
 import { Game } from '../types/Game'
@@ -108,15 +108,40 @@ function createNodesWithShapeAndStyle(graph: DefaultGraph, games: Game[]): Map<n
   return nodeMap
 }
 
-function createLikedEdges(graph: DefaultGraph, nodes: Map<number, INode>) {
-  for (const [, node] of nodes) {
+function createLikedEdges(graph: DefaultGraph, nodes: Map<number, INode>)
+{
+  for (const [, node] of nodes)
+  {
     const game = node.tag as Game
-    for (const liked_id of game.recommendations.fans_liked) {
+    for (const liked_id of game.recommendations.fans_liked)
+    {
       const likedNode = nodes.get(liked_id)
       if (!likedNode) continue
-      graph.createEdge(node, likedNode)
+      graph.createEdge(node, likedNode, new PolylineEdgeStyle(
+      {
+        stroke: '3px solid blue',
+        targetArrow: new Arrow({ fill: 'green', scale: 2, type: 'default' })
+      }))
     }
   }
 }
 
+// CustomEdgeStyle class, might need if we want to do more with edges.
+/*class CustomEdgeStyle extends EdgeStyleBase
+{
+  private createPathData(edge: IEdge): string
+  {
+    const points = IEdge.getPathPoints(edge).toArray()
+    return 'M ' + points.map(point => `${point.x} ${point.y}`).join(' L ')
+  }
 
+  protected createVisual(context: IRenderContext, edge: IEdge): Visual | null
+  {
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+    path.setAttribute('d', this.createPathData(edge))
+    path.setAttribute('fill', 'none')
+    path.setAttribute('stroke', 'black')
+    path.setAttribute('stroke-width', '1')
+    return new SvgVisual(path)
+  }  
+} */
