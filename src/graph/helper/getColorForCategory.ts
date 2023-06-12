e87790045f9868d2b0970d9773cd47fce7806c8b
Amelie Schäfer <amelie.schaefer@student.uni-tuebingen.de>
2023-06-12 17:12:22 +0200

    changed color-function and added circles for categories

--- src/graph/helper/getColorForCategory.ts
+++ src/graph/helper/getColorForCategory.ts
@@ -1,61 +1,61 @@
-import { Fill } from 'yfiles'
+import { Color } from 'yfiles'
 
-const categories = new Map<number, Fill>([
-  [1022, Fill.ORANGE], // Adventure
-  [1020, Fill.GREEN], // Exploration
-  [1010, Fill.AQUA], // Fantasy
-  [1046, Fill.AZURE], // Fighting
-  [1047, Fill.BLUE_VIOLET], // Miniatures
-  [1015, Fill.BURLY_WOOD], //Civilization
-  [1021, Fill.THISTLE], // Economic
-  [1026, Fill.CORAL], // Negotiation
-  [1001, Fill.DARK_BLUE], // Political
-  [1016, Fill.DEEP_SKY_BLUE], // Si-Fi
-  [1113, Fill.CHOCOLATE], // Space Exploration
-  [1019, Fill.DARK_CYAN], // War Game
-  [1088, Fill.SLATE_GRAY], // Industry / Manufacturing
-  [2710, Fill.DARK_SALMON], // Post Napoleonic
-  [1011, Fill.GOLD], // Transportation
-  [1084, Fill.INDIGO], // Environmental
-  [2145, Fill.GAINSBORO], // Medical
-  [1089, Fill.GRAY], // Animals
-  [1086, Fill.FIREBRICK], // Territory Building
-  [1102, Fill.GAINSBORO], // Civil War
-  [1064, Fill.BISQUE], // Movie / TV / Radio Theme
-  [1093, Fill.CADET_BLUE], // Novel Based
-  [1082, Fill.CORNFLOWER_BLUE], // Mythology
-  [1002, Fill.HOT_PINK], // Card Game
-  [1069, Fill.FUCHSIA], // Modern Warfare
-  [1055, Fill.DEEP_PINK], // American West
-  [1017, Fill.DODGER_BLUE], // Dice
-  [1035, Fill.IVORY], // Medieval
-  [1024, Fill.LAVENDER_BLUSH], // Horror
-  [1050, Fill.LIME], // Ancient
-  [1029, Fill.YELLOW_GREEN], // City Building
-  [1008, Fill.ALICE_BLUE], // Nautical
-  [1013, Fill.LAWN_GREEN], // Farming
-  [1028, Fill.FOREST_GREEN], // Puzzle
-  [1094, Fill.GOLDENROD], // Educational
-  [1044, Fill.MAROON], // Collectible Components
-  [1020, Fill.BROWN], // Exploration
-  [1097, Fill.STEEL_BLUE], // Travel
-  [1115, Fill.TAN], // Religious
-  [1116, Fill.SKY_BLUE], // Comic Books / Strip
-  [1081, Fill.CHARTREUSE], // Spy / Secret Agent
-  [1040, Fill.MAROON], // Murder / Mystery
-  [1090, Fill.TOMATO], // Pirates
-  [1032, Fill.SPRING_GREEN], // Action / Dexterity
-  [1118, Fill.SNOW], // Mature / Adult
-  [1101, Fill.TEAL], // Video Game Theme
-  [1023, Fill.RED], // Bluffing
-  [1009, Fill.DODGER_BLUE], // Abstract Strategy
-  [1070, Fill.PAPAYA_WHIP], // Renaissance
-  [1052, Fill.OLIVE], // Arabian
-  [1036, Fill.WHITE_SMOKE], // Prehistoric
-  [1039, Fill.MISTY_ROSE] // Deduction
+const categories = new Map<number, Color>([
+  [1022, Color.ORANGE], // Adventure
+  [1020, Color.GREEN], // Exploration
+  [1010, Color.AQUA], // Fantasy
+  [1046, Color.AZURE], // Fighting
+  [1047, Color.BLUE_VIOLET], // Miniatures
+  [1015, Color.BURLY_WOOD], //Civilization
+  [1021, Color.THISTLE], // Economic
+  [1026, Color.CORAL], // Negotiation
+  [1001, Color.DARK_BLUE], // Political
+  [1016, Color.DEEP_SKY_BLUE], // Si-Fi
+  [1113, Color.CHOCOLATE], // Space Exploration
+  [1019, Color.DARK_CYAN], // War Game
+  [1088, Color.SLATE_GRAY], // Industry / Manufacturing
+  [2710, Color.DARK_SALMON], // Post Napoleonic
+  [1011, Color.GOLD], // Transportation
+  [1084, Color.INDIGO], // Environmental
+  [2145, Color.GAINSBORO], // Medical
+  [1089, Color.GRAY], // Animals
+  [1086, Color.FIREBRICK], // Territory Building
+  [1102, Color.GAINSBORO], // Civil War
+  [1064, Color.BISQUE], // Movie / TV / Radio Theme
+  [1093, Color.CADET_BLUE], // Novel Based
+  [1082, Color.CORNFLOWER_BLUE], // Mythology
+  [1002, Color.HOT_PINK], // Card Game
+  [1069, Color.FUCHSIA], // Modern Warfare
+  [1055, Color.DEEP_PINK], // American West
+  [1017, Color.DODGER_BLUE], // Dice
+  [1035, Color.IVORY], // Medieval
+  [1024, Color.LAVENDER_BLUSH], // Horror
+  [1050, Color.LIME], // Ancient
+  [1029, Color.YELLOW_GREEN], // City Building
+  [1008, Color.ALICE_BLUE], // Nautical
+  [1013, Color.LAWN_GREEN], // Farming
+  [1028, Color.FOREST_GREEN], // Puzzle
+  [1094, Color.GOLDENROD], // Educational
+  [1044, Color.MAROON], // Collectible Components
+  [1020, Color.BROWN], // Exploration
+  [1097, Color.STEEL_BLUE], // Travel
+  [1115, Color.TAN], // Religious
+  [1116, Color.SKY_BLUE], // Comic Books / Strip
+  [1081, Color.CHARTREUSE], // Spy / Secret Agent
+  [1040, Color.MAROON], // Murder / Mystery
+  [1090, Color.TOMATO], // Pirates
+  [1032, Color.SPRING_GREEN], // Action / Dexterity
+  [1118, Color.SNOW], // Mature / Adult
+  [1101, Color.TEAL], // Video Game Theme
+  [1023, Color.RED], // Bluffing
+  [1009, Color.DODGER_BLUE], // Abstract Strategy
+  [1070, Color.PAPAYA_WHIP], // Renaissance
+  [1052, Color.OLIVE], // Arabian
+  [1036, Color.WHITE_SMOKE], // Prehistoric
+  [1039, Color.MISTY_ROSE] // Deduction
 ])
 
-export default function getColorForCategory(categoryId: number): Fill {
+export default function getColorForCategory(categoryId: number): Color {
   const color = categories.get(categoryId)
-  return color ?? Fill.BLACK
+  return color ?? Color.BLACK
 }
