c2f11c96b419145c25fa69f2f42a0cfb812f53f9
Amelie Schäfer <amelie.schaefer@student.uni-tuebingen.de>
2023-06-12 17:12:22 +0200

    changed color-function and added circles for categories

--- src/graph/nodes/Nodes.ts
+++ src/graph/nodes/Nodes.ts
@@ -14,33 +14,48 @@ import {
   Stroke,
   TextWrapping,
   VerticalTextAlignment,
-  StringTemplateNodeStyle
+  StringTemplateNodeStyle,
+  Fill,
+  Color
 } from 'yfiles'
 import { Game } from '../../types/Game'
 import { ReactComponent as nodeStyle } from './node-style.svg';
+import getColorForCategory from '../helper/getColorForCategory';
+
+function colorToString(color: Color) {
+  let colorString = `"rgb(` + color.r + `, ` + color.g + `, ` + color.b + `)"`
+  return colorString
+}
 
 export function createNodes(graph: DefaultGraph, games: Game[]): Map<number, INode> {
   const gameShape = new Rect(0, 0, 100, 100)
   const nodeMap = new Map<number, INode>()
 
   for (const game of games) {
-    const gameNodeStyle = new ShapeNodeStyle({
-      shape: ShapeNodeShape.ROUND_RECTANGLE, // SHAPE OF NODES
-      stroke: Stroke.BEIGE, // COLOR OF STROKE
-      fill: `rgb(19,15,135, ${(games.length + 1 - game.rank) / games.length})` // FILL COLOR WITH DESENDING RANK
-    })
 
     const amountOfCategories = game.types.categories.length
+    const categorieCircles = [
+      `<circle cx="0" cy="50" r="10"`,
+      `<circle cx="5" cy="28" r="10"`,
+      `<circle cx="20" cy="10" r="10"`,
+      `<circle cx="40" cy="0" r="10"`,
+      `<circle cx="65" cy="0" r="10"`,
+      `<circle cx="87" cy="10" r="10"`,
+      `<circle cx="101" cy="30" r="10"`
+    ]
 
-    const nodeStyle = `<g>
-    <circle cx="50" cy="50" r="50" fill="#e9e9ca" />
-    <text x="50" y="58" font-size="16" stroke="#000">${game.title}</text></g>`
-    const gameNodeStyleSVG = new StringTemplateNodeStyle(nodeStyle)
+    let nodeStyle = `<circle cx="55" cy="50" r="50" fill="#e9e9ca" />
+    <text x="55" y="58" font-size="16" text-anchor="middle" stroke="#000" data-content='{Binding title}'></text>`
+    let circles = ``
+
+    for (let c = 0; c < amountOfCategories; c++){
+      let colorOfCategory = colorToString(getColorForCategory(game.types.categories[c].id))
+      circles = circles + categorieCircles[c] + ` fill= ${colorOfCategory}/>`
+    }
+    
+    const gameNodeStyleSVG = new StringTemplateNodeStyle(circles + nodeStyle)
 
     const gameNode = graph.createGroupNode(null, gameShape, gameNodeStyleSVG, game)
-    // use a label model that stretches the label over the full node layout, with small insets
-    const centerLabelModel = new InteriorStretchLabelModel({ insets: 10 }) // STRETCHES LABEL INTO SPACE WITH *insets* PADDING
-    const centerParameter = centerLabelModel.createParameter(InteriorStretchLabelModelPosition.CENTER)
 
     const nodeLabelStyle = new DefaultLabelStyle({ // NODELABELSTYLE
       wrapping: TextWrapping.WORD, // TEXT-WRAPPING PER WORD
@@ -50,6 +65,7 @@ export function createNodes(graph: DefaultGraph, games: Game[]): Map<number, INo
       horizontalTextAlignment: HorizontalTextAlignment.CENTER, // HORIZONTAL TEXT ALIGNMENT
       clipText: false // CLIPS TEXT IF IT DOESN'T FIT
     })
+
     //graph.addLabel(gameNode, game.title, centerParameter, nodeLabelStyle) // ADDS LABEL
     nodeMap.set(game.id, gameNode)
   }
