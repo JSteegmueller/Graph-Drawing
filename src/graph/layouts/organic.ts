54cb266578362ce06b5c84cc31ff24900b021b17
Janik Steegmüller <janik@steegmueller.dev>
2023-06-13 22:56:47 +0200

    feat: increase min node distance

--- src/graph/layouts/organic.ts
+++ src/graph/layouts/organic.ts
@@ -3,7 +3,7 @@ import { ILayoutAlgorithm, OrganicLayout } from 'yfiles'
 export default function getOrganicLayout(): ILayoutAlgorithm {
   const layout = new OrganicLayout()
   layout.considerNodeSizes = true
-  layout.minimumNodeDistance = 50
+  layout.minimumNodeDistance = 100
   layout.deterministic = true
   layout.preferredMinimumNodeToEdgeDistance = 100
   layout.nodeEdgeOverlapAvoided = true
