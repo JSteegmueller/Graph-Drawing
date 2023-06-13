579d3ca4ca982b416a869f655e62beb5fe7e1821
Cornelius Bopp <Cornelius.Bopp@gmx.de>
2023-06-13 23:19:11 +0200

    added edge thickness, doesnt work yet tho, graph empty :(

--- src/graph/edges/Edges.ts
+++ src/graph/edges/Edges.ts
@@ -12,23 +12,54 @@ import {
 import { Game } from '../../types/Game'
 
 export function createEdges(graph: DefaultGraph, nodes: Map<number, INode>) {
+  const maxEdgeThickness: number = 10;
   for (const [, node] of nodes) {
     const game = node.tag as Game
-    for (const liked_id of game.recommendations.fans_liked) {
+    for (const liked_id of game.recommendations.fans_liked)
+    {
       const likedNode = nodes.get(liked_id)
+      // Map.get can return undefined->check for undefined to secure type.
+      if(likedNode === undefined)
+        throw new Error("likedNode is undefined");
+      // calculateEdgeThickness
+      // TODO: replace with CustomEdgeClass.calculateEdgeThickness
+      var similarity : number = calculateSimilarity(node, likedNode)
+      var thickness: number = Math.ceil((1/similarity) * maxEdgeThickness)
+
       if (!likedNode) continue
       graph.createEdge(node, likedNode, new PolylineEdgeStyle(
-        {
-          stroke: '3px solid blue',
-          targetArrow: new Arrow({ fill: 'green', scale: 2, type: 'default' })
-        }))
+      {
+        stroke: thickness.toString() + 'px solid blue',
+        targetArrow: new Arrow({ fill: 'green', scale: 2, type: 'default' })
+      }))
     }
   }
 }
 
+// TODO: integrate function into CustomEdgeStyle class and call it from there.
+export function calculateSimilarity(sourceNode: INode, targetNode: INode)
+{
+  const sourceGame = sourceNode.tag as Game
+  const targetGame = targetNode.tag as Game
+  var similarity_counter: number = 1
+  for (const sourceCategory of sourceGame.types.categories)
+  {
+    for (const targetCategory of targetGame.types.categories)
+    {
+      if(sourceCategory === targetCategory)
+      {
+        similarity_counter++
+      }
+    }
+  }
+  return similarity_counter
+}
+
+
 // CustomEdgeStyle class, might need if we want to do more with edges.
 class CustomEdgeStyle extends EdgeStyleBase {
-  protected createVisual(context: IRenderContext, edge: IEdge): Visual | null {
+  protected createVisual(context: IRenderContext, edge: IEdge): Visual | null
+  {
     const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
     path.setAttribute('d', this.createPathData(edge))
     path.setAttribute('fill', 'none')
