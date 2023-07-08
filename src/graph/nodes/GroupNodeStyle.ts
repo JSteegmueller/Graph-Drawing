5dc2496e302718e47f0d590becad484a731c0ff8
Nezuber <tanja-huber@t-online.de>
2023-07-08 19:12:22 +0200

    attempted to create islands, still WIP

--- src/graph/nodes/GroupNodeStyle.ts
+++ src/graph/nodes/GroupNodeStyle.ts
@@ -25,14 +25,144 @@ export function getGroupNodeStyle(graph: DefaultGraph, containedGames: IListEnum
   return groupNodeStyleSVG
 }
 
+function makeLeftTurn(p: { x: number; y: number }, q: { x: number; y: number }, r: { x: number; y: number }): boolean {
+  const p1 = p;
+  const p2 = q;
+  const p3 = r;
+  return (
+    (p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y) > 0
+  );
+}
+
+function extractCoordinates(nodes: INode[]): { x: number; y: number }[] {
+  return nodes.map(node => ({
+    x: node.layout.center.x,
+    y: node.layout.center.y
+  }));
+}
+
+function convexHull(nodes: INode[]): INode[] {
+  const coordinates = extractCoordinates(nodes);
+
+  // Sort nodes based on polar angle
+  coordinates.sort((a, b) => {
+    if (a.y === b.y) {
+      return a.x - b.x;
+    }
+    return a.y - b.y;
+  });
+
+  const stack: { x: number; y: number }[] = [];
+  stack.push(coordinates[0]);
+  stack.push(coordinates[1]);
+
+  for (let i = 2; i < coordinates.length; i++) {
+    while (
+      stack.length >= 2 &&
+      !makeLeftTurn(stack[stack.length - 2], stack[stack.length - 1], coordinates[i])
+    ) {
+      stack.pop();
+    }
+    stack.push(coordinates[i]);
+  }
+
+  const convexHullNodes: INode[] = stack.map(coord => {
+    const nodeIndex = coordinates.findIndex(c => c.x === coord.x && c.y === coord.y);
+    return nodes[nodeIndex];
+  });
+
+  return convexHullNodes;
+}
+
+function generatePathFromPolygon(nodes: INode[]): string {
+  let path = "";
+
+  if (nodes.length > 0) {
+    const numNodes = nodes.length;
+
+    const firstNode = nodes[0];
+    const firstNodeX = firstNode.layout.center.x;
+    const firstNodeY = firstNode.layout.center.y;
+
+    path += `M${firstNodeX},${firstNodeY} `;
+
+    if (nodes.length === 1) {
+      // Only one node, return a circle
+      path += `A1,1 0 1 1 ${firstNodeX + 1},${firstNodeY}`;
+    } else {
+      for (let i = 1; i < numNodes; i++) {
+        const previousNode = nodes[i - 1];
+        const previousNodeX = previousNode.layout.center.x;
+        const previousNodeY = previousNode.layout.center.y;
+
+        const currentNode = nodes[i];
+        const currentNodeX = currentNode.layout.center.x;
+        const currentNodeY = currentNode.layout.center.y;
+
+        const nextNode = nodes[(i + 1) % numNodes];
+        const nextNodeX = nextNode.layout.center.x;
+        const nextNodeY = nextNode.layout.center.y;
+
+        const curvature = 0.4 + Math.random() * 0.2; // Adjust curvature randomness
+        const controlPointX = currentNodeX + (nextNodeX - previousNodeX) * curvature;
+        const controlPointY = currentNodeY + (nextNodeY - previousNodeY) * curvature;
+
+        path += `Q${controlPointX},${controlPointY} ${currentNodeX},${currentNodeY} `;
+      }
+
+      // Add rounded endcaps
+      const lastNode = nodes[numNodes - 1];
+      const lastNodeX = lastNode.layout.center.x;
+      const lastNodeY = lastNode.layout.center.y;
+      const firstNodeX = firstNode.layout.center.x;
+      const firstNodeY = firstNode.layout.center.y;
+      path += `A2,2 0 0 1 ${lastNodeX},${lastNodeY} `;
+      path += `A2,2 0 0 1 ${firstNodeX},${firstNodeY} `;
+    }
+  }
+
+  return path;
+}
+
+function createSvgPath(nodes: INode[]): string {
+  let path = "";
+
+  if (nodes.length > 0) {
+    const convexHullNodes = convexHull(nodes);
+    path = generatePathFromPolygon(convexHullNodes);
+  }
+
+  const fill = "green";
+  const strokeWidth = 2; // Adjust the desired stroke width
+  const strokeLinejoin = "round"; // Set the strokeLinejoin attribute to round
+  const svgPath = `<path d="${path}" fill="${fill}" stroke-width="${strokeWidth}" stroke-linejoin="${strokeLinejoin}" />`;
+
+  return svgPath;
+}
+
 export function applyGroupNodeStyle(graph: DefaultGraph) {
   for (const node of graph.nodes) {
-    console.log(node.tag.title)
-    console.log(node.tag.clique)
-    console.log(node.layout.center.x)
-    console.log(node.layout.center.y)
+    // console.log(node.tag.title)
+    // console.log(node.tag.clique)
+    // console.log(node.layout.center.x)
+    // console.log(node.layout.center.y)
+    // if (graph.isGroupNode(node)) {
+    //   let groupNodeStyleSVG = getGroupNodeStyle(graph, graph.getChildren(node))
+    //   graph.setStyle(node, groupNodeStyleSVG)
+    // }
+
     if (graph.isGroupNode(node)) {
-      let groupNodeStyleSVG = getGroupNodeStyle(graph, graph.getChildren(node))
+      let nodes = Array.from(graph.getChildren(node));
+      let individualNodes = nodes.filter(n => !graph.isGroupNode(n));
+      let convexHullNodes = convexHull(individualNodes);
+
+      let padding = 5
+      let svgPath = createSvgPath(convexHullNodes);
+
+      let newpath = `<g transform="translate(-500 -1400)">`+ svgPath + `</g>`
+
+      let groupNodeStyleSVG = new StringTemplateNodeStyle(newpath);
+
       graph.setStyle(node, groupNodeStyleSVG)
     }
   }
