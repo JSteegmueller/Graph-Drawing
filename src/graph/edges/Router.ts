c52e1e2e57d6a4e66489e133daf0a4b9355d2e6d
Janik Steegmüller <janik@steegmueller.dev>
2023-07-17 10:51:18 +0200

    feat: edge ports

--- src/graph/edges/Router.ts
+++ src/graph/edges/Router.ts
@@ -1,11 +1,18 @@
-import { DefaultGraph, EdgeRouter, EdgeRouterData, PortConstraint, PortSide } from 'yfiles'
+import { DefaultGraph, EdgeRouter, EdgeRouterData, IEdge, PortConstraint, PortSide } from 'yfiles'
+import { BIDIRECTIONAL } from './Bidirectional'
 
 export function applyEdgeRouting(graph: DefaultGraph) {
   const router = new EdgeRouter()
+  router.rerouting = true
   router.polylineRouting = true
+  for (const edge of graph.edges) console.log(edge.tag)
   const layoutData = new EdgeRouterData({
-    sourcePortConstraints: edge => PortConstraint.create(PortSide.EAST),
-    targetPortConstraints: edge => PortConstraint.create(PortSide.EAST)
+    sourcePortConstraints: edge => edge.tag === BIDIRECTIONAL ? PortConstraint.create(PortSide.SOUTH) : PortConstraint.create(PortSide.EAST),
+    targetPortConstraints: edge => edge.tag === BIDIRECTIONAL ? PortConstraint.create(PortSide.SOUTH) : PortConstraint.create(PortSide.SOUTH),
+    sourceGroupIds: (edge: IEdge) => `${edge.sourceNode}${edge.tag === BIDIRECTIONAL ? 'a' : 'b'}`,
+    targetGroupIds: (edge: IEdge) => `${edge.targetNode}${edge.tag === BIDIRECTIONAL ? 'a' : 'a'}`
   })
+  router.minimumNodeToEdgeDistance = 200
+
   graph.applyLayout(router, layoutData)
 }
